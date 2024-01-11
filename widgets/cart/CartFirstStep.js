b6a782f3d172b43761491ccac7408b07aac640c3
Fer Enoch <ferenochsoames@gmail.com>
2024-01-10 21:03:18 -0300

    Hide under development delivery cost UI temporarily.

--- widgets/cart/CartFirstStep.js
+++ widgets/cart/CartFirstStep.js
@@ -3,7 +3,7 @@ import classes from './CartFirstStep.module.css'
 import { i18n } from '@/shared/model/i18n'
 import CartFooter from '@/entities/cart/ui/CartFooter'
 import AddsSection from './AddsSection'
-import DeliveryCost from './DeliveryCost'
+// import DeliveryCost from './DeliveryCost'
 
 const { CART: { FIRST_STEP_TITLE, ADDS_LABELS } } = i18n.LANG.ESP.UI
 
@@ -14,7 +14,7 @@ export default function CartFirstStep () {
       <h2 className={classes.title}>{FIRST_STEP_TITLE.toUpperCase()}</h2>
       <CartProductsSlider />
       {ADDS_LABELS.length > 0 && <AddsSection />}
-      <DeliveryCost />
+      {/* <DeliveryCost /> */}
       <CartFooter />
     </main>
   )
