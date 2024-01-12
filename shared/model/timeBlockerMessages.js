58434da1f5f19f5051eecfafc8a3b3ad1cf3f979
Fer Enoch <ferenochsoames@gmail.com>
2024-01-11 21:23:29 -0300

    Change the take-away business hours and its UI prompt

--- shared/model/timeBlockerMessages.js
+++ shared/model/timeBlockerMessages.js
@@ -3,5 +3,5 @@ export const TIME_BLOCKER = {
   DISABLED_DAY: 'Helsinki Delivery no trabaja los días lunes, martes y domingos.\nLos pedidos se agendan a partir del miércoles a las 10hs.',
   DISABLED_HOURS: 'No es posible comprar en este momento\nLos pedidos se agendan a partir de las 10hs.',
   BOOK_ORDER_NOT_DELIVERY: 'El delivery es a partir de las 20hs.\nTu pedido será agendado y tan pronto abramos, nos comunicaremos con vos para coordinar la entrega.',
-  ADD_TAKE_AWAY: 'Viernes y sábados tenemos TAKE-AWAY de 13.30 a 15.30\nPunto de encuentro: Urquiza y Cándido Pujato.\nHacé tu pedido y nos comunicaremos con vos para coordinar la entrega.'
+  ADD_TAKE_AWAY: 'Viernes y sábados tenemos TAKE-AWAY de 14.30 a 16.30\nPunto de encuentro: Urquiza y Cándido Pujato.\nHacé tu pedido y nos comunicaremos con vos para coordinar la entrega.'
 }
