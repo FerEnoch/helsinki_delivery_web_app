2edc2311f81f843aed0ab05dc396109ebbde2860
Fer Enoch <ferenochsoames@gmail.com>
2024-02-28 16:54:35 -0300

    Retrieve combos data from combos-test firestore collection.

--- processes/services/config/firebase/databases.js
+++ processes/services/config/firebase/databases.js
@@ -1,6 +1,6 @@
 export const FIREBASE_DATABASES = {
-  // PRODUCT_CATEGORIES: 'products-categories', // 'categories-test' para pruebas y debuggin
-  PRODUCT_CATEGORIES: 'categories-test',
+  PRODUCT_CATEGORIES: 'products-categories', // 'categories-test' para pruebas y debuggin
+  // PRODUCT_CATEGORIES: 'categories-test',
   PRODUCT_COMBOS: 'combos-test',
   // PRODUCT_COMBOS: 'products-combos', // 'combos-test' para pruebas y debuggin
   INFO: 'info',
