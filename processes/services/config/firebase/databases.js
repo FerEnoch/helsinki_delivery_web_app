14ca0b97f2a316da5c426b6bb528bd83d668f1fc
Fer Enoch <ferenochsoames@gmail.com>
2024-03-10 18:16:08 -0300

    Switch to production combos db

--- processes/services/config/firebase/databases.js
+++ processes/services/config/firebase/databases.js
@@ -1,8 +1,10 @@
 export const FIREBASE_DATABASES = {
-  PRODUCT_CATEGORIES: 'products-categories', // 'categories-test' para pruebas y debuggin
+  /* - 'categories-test' para pruebas y debuggin - */
+  PRODUCT_CATEGORIES: 'products-categories',
   // PRODUCT_CATEGORIES: 'categories-test',
-  PRODUCT_COMBOS: 'combos-test',
-  // PRODUCT_COMBOS: 'products-combos', // 'combos-test' para pruebas y debuggin
+  /* - 'combos-test' para pruebas y debuggin - */
+  PRODUCT_COMBOS: 'products-combos',
+  // PRODUCT_COMBOS: 'combos-test',
   INFO: 'info',
   FAQ: 'faq',
   PAYMENT_METHODS: 'paymentMethods'
