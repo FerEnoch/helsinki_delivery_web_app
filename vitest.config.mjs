832090bf127d582af9226fd7421ae7a4b610a7d2
Fer Enoch <ferenochsoames@gmail.com>
2024-03-11 20:59:49 -0300

    Update vitest.config to includes ".js"  files as ".jsx"

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vitest/config'
+import { transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform (code, id) {
+        if (!id.match(/.*\.js$/)) return null
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic'
+        })
+      }
+    },
+    react()
+  ],
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  },
   test: {
     environment: 'happy-dom'
   },
