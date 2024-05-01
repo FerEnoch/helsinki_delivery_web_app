3d1379db5fdd08679f0e62b9ceda333c0d50b674
Fer Enoch <ferenochsoames@gmail.com>
2024-05-01 18:26:07 -0300

    Change(UI): not business days & disabled hours messages

--- shared/model/timeBlockerMessages.js
+++ shared/model/timeBlockerMessages.js
@@ -2,7 +2,7 @@ export const TIME_BLOCKER = {
   CONTINUE: 'Continuar',
   MIDDAY_TAKE_AWAY_POSSIBLE: 'Es posible el take away de la siesta', // This message is used, but not rendered on client
   NO_TAKE_AWAY: 'No tenemos take away en este momento',
-  DISABLED_DAY: 'No trabajamos los días lunes, martes y domingos.\nLos pedidos se agendan a partir del miércoles a las 10hs.',
-  DISABLED_HOURS: 'No es posible comprar en este momento\nLos pedidos se agendan a partir de las 10hs.',
-  BOOK_ORDER_NOT_DELIVERY: 'El delivery es a partir de las 20hs.\nTu pedido será agendado y tan pronto abramos, nos comunicaremos con vos para coordinar la entrega.'
+  DISABLED_DAY: 'No es posible realizar pedidos en este momento.',
+  DISABLED_HOURS: 'No es posible realizar pedidos en este momento\nLos pedidos se agendan a partir de las 10hs.',
+  BOOK_ORDER_NOT_DELIVERY: 'Estás fuera del horario de delivery\nTu pedido será agendado y tan pronto abramos, nos comunicaremos con vos para coordinar la entrega.'
 }
